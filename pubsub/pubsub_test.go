85dbda7b2a733ae364fe43c1762dda2a35bcc6dc
cgcgbcbc <cgcgbcbc@163.com>
2015-08-06 20:44:50 +0800

    make test package the same with source

--- pubsub/pubsub_test.go
+++ pubsub/pubsub_test.go
@@ -1,17 +1,15 @@
-package pubsub_test
+package pubsub
 
 import (
 	"testing"
-
-	"github.com/mulab/TUNet-build-server/pubsub"
 )
 
 func TestPubsub(t *testing.T) {
 	//	compile time assertion
 	//	var _ pubsub.Publisher = (*pubsub.Pubsub)(nil)
 	//	run time check, both is ok
-	var p interface{} = &pubsub.Pubsub{}
-	if _, ok := p.(pubsub.MessageQueue); !ok {
+	var p interface{} = &Pubsub{}
+	if _, ok := p.(MessageQueue); !ok {
 		t.Fatal("Pubsub does not implement MessageQueue")
 	}
 }
